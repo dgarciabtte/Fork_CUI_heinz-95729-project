cadcbff70762ad62d2f2f8346bcf0effc3a1ca3b
Eric Hu <yihu2018@yahoo.com>
2023-11-29 14:43:05 -0500

    added rag for csv

--- final_project/final/chat/api/services.py
+++ final_project/final/chat/api/services.py
@@ -1,12 +1,21 @@
 from langchain.agents import create_sql_agent
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.agents.agent_types import AgentType
-from langchain.llms.openai import OpenAI
+#from langchain.llms.openai import OpenAI
 from langchain.sql_database import SQLDatabase
 
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+from langchain.document_loaders.csv_loader import CSVLoader
+import os
+
+os.environ['OPENAI_API_KEY'] = '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'
 
 
-def RAG_response(user_question):
+
+def RAG_response_db(user_question):
     db = SQLDatabase.from_uri("sqlite:////Users/erichu/Desktop/final_project/final/db.sqlite3")
     toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
 
@@ -19,3 +28,21 @@ def RAG_response(user_question):
     
     output = agent_executor.run(user_question)
     return output
+
+def rag_csv(query):
+    
+    loader = CSVLoader("./book_dataset.csv")
+    docs = loader.load()
+
+    embeddings = OpenAIEmbeddings()
+  
+    vectorstore = FAISS.from_documents(docs, embeddings)
+
+    vectorstore.save_local("faiss_index_constitution")
+
+    persisted_vectorstore = FAISS.load_local("faiss_index_constitution", embeddings)
+
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=persisted_vectorstore.as_retriever())
+    result = qa.run(query)
+    print(result)
+    return result
--- final_project/final/chat/api/views.py
+++ final_project/final/chat/api/views.py
@@ -2,12 +2,13 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from .services import RAG_response
+from .services import rag_csv
 from rest_framework.response import Response
 
 class ChatResponseSet(viewsets.ViewSet):
     
     @action(methods=["POST"],detail=False)
-    def answer_user_question(self, request):
+    def answer_question(self, request):
         user_question = request.data.get("user_input")
-        response = RAG_response(user_question)
+        response = rag_csv(user_question)
         return Response({"message": "hello world"})
